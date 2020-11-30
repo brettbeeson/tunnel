d3fe4c7fbcafc0e40da77071ef566e58b86d3e20
brettbeeson <brettbeeson@fastmail.com>
2020-11-30 17:32:50 +1000

    Rename

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nuttel",
-    version="0.0.1",
+    name="tunnel",
+    version="0.0.2",
     author="Brett Beeson",
     author_email="brettbeeson@fastmail.com",
     include_package_data=True,
     description="SSH Tunnel Utilties",
     long_description="Client makes ssh port to server at unknown port, server writes ssh config to get back to client",
     # long_description_content_type="text/markdown",
-    url="https://github.com/brettbeeson/nuttel",
+    url="https://github.com/brettbeeson/tunnel",
     packages=setuptools.find_packages(),
-    scripts = ['nuttel.py'],
+    scripts = ['tunnel.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
